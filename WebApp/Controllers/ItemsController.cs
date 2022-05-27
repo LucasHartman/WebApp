f85169b49fd14aa2c4d5e195a2c2a26ae124f835
LucasHartman <ljh.hartman@gmail.com>
2022-05-27 11:39:05 +0200

    setup

---
+++ WebApp/Controllers/ItemsController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly InMemItemsRepository repository; // get repo
+
+        public ItemsController()
+        { // Constructor 
+            repository = new InMemItemsRepository(); // Initialize Repo
+        }
+
+        // GET / items
+        [HttpGet]
+        public IEnumberable<Item> GetItems()
+        { // Get all Items
+            var items = repository.GetItems(); // initialize method from Repo
+            return items;
+        }
+
+    }
+}
