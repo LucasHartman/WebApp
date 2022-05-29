0722e83c8aa27bedef171b173797c9ea419d9203
LucasHartman <ljh.hartman@gmail.com>
2022-05-29 11:57:11 +0200

    updated code mistakes with namespaces, this version works

--- WebApp/Controllers/ItemsController.cs
+++ WebApp/Controllers/ItemsController.cs
@@ -1,24 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using WebApp.Model;
+using WebApp.Repo;
 
-namespace WebApp.Controllers;
-
-[ApiController]
-[Route("items")]
-public class ItemsController : ControllerBase
+namespace WebApp.Controllers
 {
-    private readonly ItemsRepo repository; // get repo
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly InMemItemsRepository repository; // get repo
 
-    public ItemsController()
-    { // Constructor 
-        repository = new InMemItemsRepository(); // Initialize Repo
-    }
+        public ItemsController()
+        { // Constructor 
+            repository = new InMemItemsRepository(); // Initialize Repo
+        }
 
-    // GET / items
-    [HttpGet]
-    public IEnumberable<Item> GetItems()
-    { // Get all Items
-        var items = repository.GetItems(); // initialize method from Repo
-        return items;
+        // GET / items
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        { // Get all Items
+            var items = repository.GetItems(); // initialize method from Repo
+            return items;
+        }
+
     }
 }
--- WebApp/Repo/ItemsRepo.cs
+++ WebApp/Repo/ItemsRepo.cs
@@ -1,25 +1,28 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using WebApp.Model;
 
-namespace WebApp.Repo;
-
-public class ItemsRepo
+namespace WebApp.Repo
 {
+    public class InMemItemsRepository
+    {
+        private readonly List<Item> items = new()
+        {   // initialize a list of items
+            new Item { Id= Guid.NewGuid(), Name= "Potion", Price=9, CreatedDate=DateTimeOffset.UtcNow},
+            new Item { Id= Guid.NewGuid(), Name= "Iron Swords", Price=20, CreatedDate=DateTimeOffset.UtcNow},
+            new Item { Id= Guid.NewGuid(), Name= "Bronze Shield", Price=18, CreatedDate=DateTimeOffset.UtcNow}
+        };
 
-    private readonly List<Item> items = new()
-    {   // initialize a list of items
-        new Item { Id= Guide.NewGuid(), nameof= "Potion", Price=9, CreatedDate=DateTimeOffSet.UTcNow},
-        new Item { Id= Guide.NewGuid(), nameof= "Iron Swords", Price=20, CreatedDate=DateTimeOffSet.UTcNow},
-        new Item { Id= Guide.NewGuid(), nameof= "Bronze Shield", Price=18, CreatedDate=DateTimeOffSet.UTcNow}
-    };
+        public IEnumerable<Item> GetItems()
+        {   // get all items
+            return items;
+        }
 
-    public IEnumberable<Item> GetItems()
-    {   // get all items
-        return items;
-    }
+        public Item GetItem(Guid id)
+        {   // get a single item bases on id
+            return items.Where(item => item.Id==id).SingleOrDefault();
+        }
 
-    public Item GetItem(Guide id)
-    {   // get a single item bases on id
-        return items.Where(item => item.id==id).SingleOrDefault();
     }
 }
